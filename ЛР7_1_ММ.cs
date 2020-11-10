5d0cb8b64fb57bd8e6664e50b98679657503802b
NiKOLa1477 <48329607+NiKOLa1477@users.noreply.github.com>
2020-11-10 20:22:12 +0200

    Add files via upload

---
+++ ЛР7_1_ММ.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace ST_LW3
+{
+    class Program
+    {
+        public static int N = 6;        
+        static void Main(string[] args)
+        {
+            int[,] C = InitC();
+            Console.WriteLine("C = C1");
+            Print(C);            
+            int[,] C1 = InitCn(C);
+            Console.WriteLine("C2");
+            Print(C1);
+            int k = 2;
+            if(Equal(C, C1) == false)
+            {
+                int[,] C2 = InitCn(C1);
+                Console.WriteLine($"C{Math.Pow(2, k)}");
+                Print(C2);
+                while (Equal(C2, C1) == false)
+                {
+                    k++;
+                    C1 = C2;                    
+                    C2 = InitCn(C1);
+                    Console.WriteLine($"C{Math.Pow(2, k)}");
+                    Print(C2);
+                }
+                C1 = C2;
+            }
+            Console.WriteLine($"C* = C{Math.Pow(2, k-1)}");
+            Print(C1);
+
+            int start, end;
+            start = 1;
+            end = 2;
+            int[] Way = Min(C, C1, start - 1, end - 1);
+            Console.WriteLine();
+            Console.WriteLine($"Найкоротший шлях з х{start} в x{end} = {C1[start - 1, end - 1]}");
+            for(int i=0;i<N;i++)
+            {
+                if(Way[i]!=0 || i == 0)
+                {
+                    if (Way[i] != end - 1)
+                        Console.Write($"x{Way[i] + 1} -> ");
+                    else
+                        Console.WriteLine($"x{ Way[i] + 1}");
+                }
+
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        public static int[,] InitCn(int[,] X)
+        {
+            int[,] C = new int[N, N];
+            for(int i=0;i<N;i++)
+            {
+                for(int j=0;j<N;j++)
+                {
+                    if(X[i,j]>2)
+                    {
+                        C[i, j] = Min(X, i, j);
+                    }
+                    else
+                    {
+                        C[i, j] = X[i, j];
+                    }
+                }
+            }
+            return C;
+        }
+        public static int[,] InitC()
+        {
+            int M = 5 * N;
+            int[,] C = { { 0, M, 1, M, 1, 4 }, { M, 0, M, 2, 5, 2 }, { 1, M, 0, 2, M, 1 }, { M, 2, 2, 0, M, 3 }, { 1, 5, M, M, 0, 2 }, { 4, 2, 1, 3, 2, 0 } };
+            return C;
+        }
+        public static int Min(int[,] X, int a, int b)
+        {
+            int min = 5 * N;            
+            for (int k = 0; k < N; k++)
+            {
+                if (min > X[a, k] + X[k, b])
+                {
+                    min = X[a, k] + X[k, b];
+                }
+            }
+            return min;
+        }
+        
+        public static int[] Min(int[,] X, int[,] Y, int a, int b)
+        {
+            int j = 1;
+            int[] way = new int[N];
+            way[0] = a;
+            for (int i = 0; i < N; i++)
+            {
+                if (X[a, i] + Y[i, b] == Y[a, b] && i != a)
+                {
+                    way[j] = i;
+                    j++;
+                    a = i;
+                }
+            }
+            while(way[j-1]!=b)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (X[a, i] + Y[i, b] == Y[a, b] && i != a)
+                    {
+                        way[j] = i;
+                        j++;
+                    }
+                }
+            }
+            return way;                       
+        }
+        public static void Print(int[,] X)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"{X[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool Equal(int[,] X, int[,] Y)
+        {
+            bool equal = true;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (X[i, j] != Y[i, j])
+                    {
+                        equal = false;
+                    }
+
+                }
+            }
+            return equal;
+        }
+        
+    }
+}
---
+++ ЛР7_2_ММ.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace ЛР7_2_ММ
+{
+    class Program
+    {
+        public static int N = 10;
+        static void Main(string[] args)
+        {
+            int[,] C = new int[N, N];
+            C = InitC();
+            Console.WriteLine("C");
+            Print(C, N, N);
+            Console.WriteLine();
+            bool[] I = new bool[N];
+            int start = 1;
+            int end = 2;
+            int[,] min = new int[N, 2];
+            I[start - 1] = true;            
+            min[start - 1, 0] = 0;
+            min[start - 1, 1] = 0;
+            int[] t = Min(C, min, I);
+            min[t[0]-1, 0] = t[1];
+            min[t[0]-1, 1] = t[2];
+            I[t[0] - 1] = true;                        
+            while (Check(I) == false)
+            {
+                t = Min(C, min, I);
+                min[t[0] - 1, 0] = t[1];
+                min[t[0] - 1, 1] = t[2];
+                I[t[0] - 1] = true;                                
+            }
+            Console.WriteLine("n h (мiнiмальний шлях(h), попередня вершина(n))");
+            Print(min, N, 2);
+
+            int[] Way = new int[N];
+            int temp = end - 1;
+            for(int i=0;i<N;i++)
+            {
+                if (i == 0)
+                    Way[i] = end;
+                else if (min[temp, 0] == start)
+                {
+                    Way[i] = start;
+                    break;
+                }
+                else
+                {
+                    Way[i] = min[temp, 0];
+                    temp = min[temp, 0] - 1;
+                }
+            }
+            Console.WriteLine();
+            Print(Way, N);
+            Console.WriteLine($"h = {min[end - 1, 1]}");
+            Console.ReadKey();
+        }
+
+        public static bool Check(bool[] I)
+        {
+            bool check = true;
+            for (int i = 0; i < N; i++)
+            {
+                if (I[i] == false)
+                    check = false;
+            }
+            return check;
+        }
+        public static int[] Min(int[,] C, int[,] min, bool[] I)
+        {
+            int[,] h = new int[N, N];
+            int[] temp = new int[3];
+            temp[1] = 0;
+            for(int i=0;i<N;i++)
+            {
+                if(I[i]==true)
+                {                    
+                    for(int j=0;j<N;j++)
+                    {
+                        if(I[j]==false && C[i, j] > 0)
+                        {
+                            h[i, j] = min[i, 1] + C[i, j];                           
+                            if(temp[2]>h[i,j] || temp[2] == 0)
+                            {
+                                temp[0] = j + 1;
+                                temp[1] = i + 1;
+                                temp[2] = h[i, j];
+                            }
+                            
+                        }
+                    }
+                }
+            }            
+            return temp;
+        }
+        public static int[,] InitC()
+        {
+            int[,] C = new int[N, N];
+            C[0, 5] = 2;
+            C[0, 6] = 3;
+            C[0, 7] = 1;
+            C[0, 8] = 5;
+            C[2, 1] = 5;
+            C[3, 1] = 4;
+            C[3, 4] = 1;
+            C[4, 1] = 4;
+            C[5, 3] = 3;
+            C[5, 4] = 2;
+            C[5, 6] = 5;
+            C[5, 9] = 1;
+            C[6, 1] = 6;
+            C[6, 2] = 2;
+            C[6, 4] = 1;
+            C[6, 8] = 1;
+            C[7, 6] = 2;
+            C[8, 5] = 2;
+            C[9, 3] = 2;
+            return C;
+        }
+        public static void Print(int[,] X, int m, int n)
+        {
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    Console.Write($"{X[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Print(int[] X, int n)
+        {
+            for (int i = n-1; i >= 0; i--)
+            {        
+                if(X[i] != 0)
+                    Console.Write($"x{X[i]} -> ");                               
+            }
+        }
+    }
+}
